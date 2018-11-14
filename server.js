570f168d17425a9d38fdbb1616d02fef03b20431
Fayvor George <phavorsparks@gmail.com>
2018-11-14 23:35:06 +0100

    setup hbs and static rendering

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+app.set('voew engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>')
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMsg: 'Welcome to the web-server home',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Your request was bad'
+  })
+})
+
+app.listen(3000, () => {
+  console.log(`server is running on port 3000`)
+})
