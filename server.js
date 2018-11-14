4571a49826215fe1c81ff19bbe22713203917fe9
Fayvor George <phavorsparks@gmail.com>
2018-11-15 00:11:22 +0100

    add web request logger, maintenance checker

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 const app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('voew engine', 'hbs')
+
+// middlewares
+app.use((req, res, next) => {
+  let now = new Date().toString()
+  let log = `${now}: ${req.method} : ${req.url}`
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+// Run this only in maintenance mode
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// })
+
 app.use(express.static(__dirname + '/public'))
 
+// register helpers
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 })
@@ -15,6 +37,7 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase()
 })
 
+// basic routes
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>')
   res.render('home.hbs', {
