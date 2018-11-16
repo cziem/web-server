5c24fc661a34078c57ff3b7494aa496919af1578
Fayvor George <phavorsparks@gmail.com>
2018-11-16 02:11:22 +0100

    update home.hbs

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -5,7 +5,6 @@ const router = express.Router()
 router.get('/', (req, res) => {
   res.render('default.hbs', {
     logo: 'serverify',
-    pageTitle: 'welcome to serverify',
     welcomeMsg: 'Welcome to the web-server home',
   })
 })
